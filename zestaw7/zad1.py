88b34de59fd12698b92f0fb0ab08d0892aa77c47
Czykii <czykierdak407@gmail.com>
2023-04-26 13:42:19 +0200

    Zestaw 7 praca na zajęciach

---
+++ zestaw7/zad1.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse
+import sys
+from bs4 import BeautifulSoup
+
+def main():   
+    url = urllib.request.urlopen("http://th.if.uj.edu.pl/")
+    if(url.getcode() == 200):
+        if(urllib.parse.urlparse("http://th.if.uj.edu.pl/").scheme == 'html'):
+            soup = BeautifulSoup(url, 'html.parser')            
+            text = soup.get_text()
+            if "Institute of Theorethical Physics" in text:
+                print("done")
+                sys.exit(0)      
+            else:
+                sys.exit(1)    
+        else:
+            sys.exit(1)         
+    else:
+        sys.exit(1)
+        
+main()            
