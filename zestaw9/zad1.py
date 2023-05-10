80ed048fea28d66a043b3b5c40cdb4803af03f73
Czykii <czykierdak407@gmail.com>
2023-05-10 13:45:58 +0200

    Zestaw 9 praca na zajęciach

--- zestaw9/zad1.py
+++ zestaw9/zad1.py
@@ -3,18 +3,38 @@ import sys
 from bs4 import BeautifulSoup
 
 def main():   
-    url = urllib.request.urlopen("http://th.if.uj.edu.pl/")
-    if(url.getcode() == 200):
+    url = urllib.request.urlopen("https://pogoda.interia.pl/prognoza-dlugoterminowa-krakow,cId,4970")
+    
+    if(url.getcode() == 200):    
         if(url.info().get_content_type() == 'text/html'):
             soup = BeautifulSoup(url, 'html.parser')            
-            text = soup.get_text()
-            if "Institute of Theorethical Physics" in text:
-                #print("done")
-                sys.exit(0)      
+            weather_tag = soup.find("div",class_="weather-currently-temp-strict")
+            #print(weather_tag)
+            if weather_tag is not None:
+                if weather_tag.parent is not None and weather_tag.parent.name == "div":
+                    #print(weather_tag.parent.name) 
+                    weather = weather_tag.text 
+                    if "°C" in weather:
+                        temperature = weather.strip("°C")
+                        temperature = int(temperature)
+                        if temperature > -30 and temperature < 40:
+                            
+                            print(weather)
+                            sys.exit(0)
+                        else:
+                            print("Bad temperature")
+                            sys.exit(1)    
+                    else:
+                        print("There isn't a tempereture in °C")
+                        sys.exit(1)
+                else:
+                    sys.exit(1)
             else:
-                sys.exit(1)    
+                print("Div not found")
+                sys.exit(1)                           
         else:
-            sys.exit(1)         
+            sys.exit(1)    
+    
     else:
         sys.exit(1)
         
