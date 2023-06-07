49a2bca875fac81b752bde7c673b8f010a9de8e7
Czykii <czykierdak407@gmail.com>
2023-06-07 13:38:36 +0200

    Update README i zestaw11

--- zestaw11/zad1.py
+++ zestaw11/zad1.py
@@ -2,6 +2,8 @@ import requests
 import sys
 import json
 
+# Budka Suflera - 359282
+
 def get_artists(band_id):
     url = f'https://api.discogs.com/artists/{band_id}'
     headers = {'User-Agent': 'DiscogsAppMusic/1.0'}
@@ -13,6 +15,7 @@ def get_artists(band_id):
    
 def check(id_arg, name, url):
     headers = {'User-Agent': 'DiscogsAppMusic/1.0'}
+    
     page = requests.get(url, headers)
     content = page.content.decode('utf8')
     data = json.loads(content)
